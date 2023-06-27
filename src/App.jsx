e72ceb9a177421fa0a0aa7253c0b993ed5181eb0
Derrick Maina <mburumaina648@gmail.com>
2023-06-27 12:33:06 +0300

    added context provider

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,22 @@ import Movies from "./Components/Movies"
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom"
 import Shows from "./Components/Shows"
 import Home from "./Components/Home"
+import { MovieDbProvider } from "./Context/MovieDbContext"
+
 
 function App() {
   return (
-
-    <Router> 
-     
+    <MovieDbProvider>
+    <Router>      
     <Header/>
-
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/movies" element={<Movies/>}/>
-      <Route path="/shows" element={<Shows/>}/>     
-
+      <Route path="/shows" element={<Shows/>}/> 
 
     </Routes>
-
     </Router>
+    </MovieDbProvider>
   )
 }
 export default App
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,6 +1,26 @@
+import movieDbContext from "../Context/MovieDbContext";
+import {  useEffect, useContext } from "react";
+
+
 function Movies() {
+
+   const {list, fetchdata} = useContext(movieDbContext)
+
+   useEffect(()=>{
+
+      fetchdata('movie/popular')
+
+   },[])
+
+
+
   return (
-    <div>Movies</div>
+    <div> 
+      {list.map((result)=>{
+         return <li key={result.id}>{result.original_title}</li>
+      })}    
+
+    </div>
   )
 }
 export default Movies
