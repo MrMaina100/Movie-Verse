7aa255919b769de1b4e898d0b6077b6cd003e362
Derrick Maina <mburumaina648@gmail.com>
2023-06-28 12:00:35 +0300

    displayed popular movies from context

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,32 @@
+import { useContext, useEffect } from "react"
+import movieDbContext from "../Context/MovieDbContext"
+import Search from "./Search"
+
 function Home() {
+
+  const {list,fetchdata} = useContext(movieDbContext)
+
+  useEffect(()=>{
+
+    fetchdata('movie/popular')
+
+
+  },[])
+
+
   return (
-    <div>this is home</div>
+    <div>
+      <Search/>
+      {list.map((items)=>{
+        return(
+          <li key={items.id}>{items.title}</li>
+        )
+      })}
+    </div>
+  
+
+   
   )
 }
 export default Home
+
