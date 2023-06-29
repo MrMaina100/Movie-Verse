ce0e6ef0438259ded31831ade823f65c6e755ecb
Derrick Maina <mburumaina648@gmail.com>
2023-06-29 11:36:22 +0300

    displayed popular movies

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -15,11 +15,19 @@ function Home() {
 
 
   return (
-    <div>
-      <Search/>
+  
+    <div className="flex flex-col justify-between flex-wrap items-center p-12  space-y-4 md:flex-row">
+      
       {list.map((items)=>{
-        return(
-          <li key={items.id}>{items.title}</li>
+        return(          
+             <div key={items.id}  className=" w-64 rounded-xl shadow-2xl h-96 ">
+            <img src={`https://image.tmdb.org/t/p/w500/${items.poster_path}`} alt="" className="max-h-[82%] w-full " />
+           
+            <p>{items.title}</p>
+            <p>{items.vote_average}/10</p>    
+         
+          </div>         
+         
         )
       })}
     </div>
