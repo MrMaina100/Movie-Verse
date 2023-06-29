52361e533f338577d248c43e8b3865b6e444f54f
Derrick Maina <mburumaina648@gmail.com>
2023-06-29 12:17:26 +0300

    displayed top rated movies

--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,26 +1,31 @@
-import movieDbContext from "../Context/MovieDbContext";
-import {  useEffect, useContext } from "react";
-
+import movieDbContext from "../Context/MovieDbContext"
+import { useContext, useEffect } from "react"
 
 function Movies() {
 
-   const {list, fetchdata} = useContext(movieDbContext)
+  const {list, fetchdata} = useContext(movieDbContext);
+  useEffect(()=>{
+    fetchdata('movie/top_rated');
 
-   useEffect(()=>{
-
-      fetchdata('movie/popular')
-
-   },[])
-
-
+  },[])
 
   return (
-    <div> 
-      {list.map((result)=>{
-         return <li key={result.id}>{result.original_title}</li>
-      })}    
-
+    <div className="flex flex-col justify-between flex-wrap items-center p-12  space-y-4 md:flex-row">
+      
+      {list.map((items)=>{
+        return(          
+             <div key={items.id}  className=" w-64 rounded-xl shadow-2xl h-96 ">
+            <img src={`https://image.tmdb.org/t/p/w500/${items.poster_path}`} alt="" className="max-h-[82%] w-full " />
+           
+            <p>{items.title}</p>
+            <p>{items.vote_average}/10</p>    
+         
+          </div>         
+         
+        )
+      })}
     </div>
+  
   )
 }
 export default Movies
