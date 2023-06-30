f34909e4cb6a47c185812e5b47bf5f878da06011
Derrick Maina <mburumaina648@gmail.com>
2023-06-30 09:33:44 +0300

    moved the display results to specific components

---
+++ src/Components/Display/TvDisplay.jsx
@@ -0,0 +1,31 @@
+import { useContext, useEffect } from "react"
+import movieDbContext from "../../Context/MovieDbContext"
+
+function TvDisplay() {
+
+   const {list, fetchdata} = useContext(movieDbContext)
+   useEffect(()=>{
+
+      fetchdata('tv/top_rated')
+
+   },[])
+  return (
+    
+    <div className="flex flex-col justify-between flex-wrap items-center p-12  space-y-4 md:flex-row">
+      
+      {list.map((items)=>{
+        return(          
+             <div key={items.id}  className=" w-64 rounded-xl shadow-2xl h-96 ">
+            <img src={`https://image.tmdb.org/t/p/w500/${items.poster_path}`} alt="" className="max-h-[82%] w-full " />
+           
+            <p>{items.name}</p>
+            <p>{items.vote_average}/10</p>    
+         
+          </div>         
+         
+        )
+      })}
+    </div>
+  )
+}
+export default TvDisplay
