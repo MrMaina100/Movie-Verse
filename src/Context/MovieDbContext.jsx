10b39aea64df8a3b8b9101be0d5cc0acc3f751ee
Derrick Maina <mburumaina648@gmail.com>
2023-07-01 10:13:31 +0300

    few changes

--- src/Context/MovieDbContext.jsx
+++ src/Context/MovieDbContext.jsx
@@ -1,64 +1,62 @@
-import { createContext, useState } from "react";
+import { createContext, useState,  } from "react";
 
 const movieDbContext = createContext();
 
-export const MovieDbProvider = ({ children }) => {
-  const APIKEY = import.meta.env.VITE_API;
-  const APIURL = "https://api.themoviedb.org/3/";
 
-  const [list, setList] = useState([]);
-  const [movies, setmovies] = useState([]);
-  const [series, setseries] = useState([]);
-  const [loading, setLoading] = useState(false);
+export const MovieDbProvider = ({children})=>{
+   
+const APIKEY = '';
+const APIURL = 'https://api.themoviedb.org/3/';
 
-  const searchMovie = async (text) => {
-    setLoading(true);
-    const res = await fetch(
-      `${APIURL}/search/movie?query=${text}&api_key=${APIKEY}`
-    );
-    const data = await res.json();
-    console.log(data.results);
+const [list, setList] = useState([])
 
-    setmovies(data.results);
-    setLoading(false);
-  };
 
-  const searchTv = async (text) => {
-    setLoading(true);
-    const res = await fetch(
-      `${APIURL}/search/tv?query=${text}&api_key=${APIKEY}`
-    );
-    const data = await res.json();
-    console.log(data.results);
+const searchMovie = async (text)=>{  
+   
+  setList([])
 
-    setseries(data.results);
-    setLoading(false);
-  };
+   const res = await fetch(`${APIURL}/search/movie?query=${text}&api_key=${APIKEY}`)
+   const data = await res.json()
+   console.log(data.results);
 
-  const fetchdata = async (endpoint) => {
-    const res = await fetch(
-      `${APIURL}${endpoint}?api_key=${APIKEY}&language=en-US`
-    );
-    const data = await res.json();
+   setList(data.results);
 
-    setList(data.results);
-  };
+}
 
-  return (
-    <movieDbContext.Provider
-      value={{
-        loading,
-        list,
-        fetchdata,
-        searchMovie,
-        searchTv,
-        movies,
-        series,
-      }}
-    >
-      {children}
-    </movieDbContext.Provider>
-  );
-};
+const searchTv = async (text)=>{
+   setList([])
+   const res = await fetch(`${APIURL}/search/tv?query=${text}&api_key=${APIKEY}`)
+   const data = await res.json()
+   console.log(data.results);
 
-export default movieDbContext;
+   setList(data.results)
+}
+
+
+const fetchdata = async (endpoint)=>{
+
+     const res = await  fetch(`${APIURL}${endpoint}?api_key=${APIKEY}&language=en-US`)
+     const data = await res.json()
+
+     setList(data.results);   
+   }
+
+
+
+   return (
+   <movieDbContext.Provider value={{
+      list,
+      fetchdata,
+      searchMovie,
+      searchTv
+
+   }}>       
+
+  {children}
+
+
+   </movieDbContext.Provider>
+   )
+}
+
+export default movieDbContext
