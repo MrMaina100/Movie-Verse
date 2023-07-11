cb5c01961f452c2bfa409b2389cf027bbd700408
Derrick Maina <mburumaina648@gmail.com>
2023-07-11 21:14:27 +0300

    added popular movies to the display page

--- src/Components/Display/MovieDisplay.jsx
+++ src/Components/Display/MovieDisplay.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect } from "react";
 import movieDbContext from "../../Context/MovieDbContext";
+import { Link } from "react-router-dom";
 
 
 function MovieDisplay() {
@@ -16,7 +17,9 @@ function MovieDisplay() {
      
       {apiData.map((items) => {
         return (
-          <div key={items.id} className=" w-64 rounded-xl shadow-2xl h-96 ">
+
+          <Link to={`/movieDetails/${items.id}`} key={items.id}>
+           <div key={items.id} className=" w-64 rounded-xl shadow-2xl h-96 ">
            {items.poster_path ? <img src={`https://image.tmdb.org/t/p/w500/${items.poster_path}`} alt="" className="max-h-[82%] w-full " /> 
               :
                <img src="https://www.shutterstock.com/image-vector/doodle-stick-figure-no-search-600w-1595649697.jpg" alt="" className="max-h-[82%] w-full h-full"  />  }
@@ -24,6 +27,9 @@ function MovieDisplay() {
             <p>{items.title}</p>
             <p>{items.vote_average}/10</p>
           </div>
+          
+          </Link>
+         
         );
       })
       
