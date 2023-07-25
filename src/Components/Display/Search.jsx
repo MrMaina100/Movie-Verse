4963474e37172bad5785739685e1411172dafc4a
Derrick Maina <mburumaina648@gmail.com>
2023-07-25 13:48:15 +0300

    added radio buttons for searching either for a tv or movie

--- src/Components/Display/Search.jsx
+++ src/Components/Display/Search.jsx
@@ -1,50 +1,92 @@
-import { useState, useContext } from "react"
+import { useState, useContext, useEffect } from "react"
 import movieDbContext from "../../Context/MovieDbContext"
 import { useNavigate} from "react-router-dom"
 import { toast } from "sonner"
 
 function Search() {
 
-   const [formData, setFormData] = useState('') 
+   const [formData, setFormData] = useState({
+    searchInput:'',
+    type:'',
+
+   }) 
+   const {searchInput, type} = formData
+  
+
    const navigate = useNavigate()
    const { searchData } = useContext(movieDbContext)
+    
+
+   const handleChange = (e)=>{
+    setFormData((prevState)=>({
+      ...prevState,
+      [e.target.name]:e.target.value
+    }))
+
+   }
 
    const handleSubmit = (e)=>{
       e.preventDefault()
-      if(formData === ''){
+      if(searchInput.trim() === ''){
         toast.error('please input something')
       }else{
-        searchData(formData)
-        navigate('/searchResults')        
-        setFormData('')
+        searchData(searchInput, type)
+          
+        setFormData({
+         searchInput:'',
+         type:'',
+        })
 
       }
-    
+        {type === 'movie' ? navigate('/MovieSearchResults'): navigate('/TvSearchResults')}    
+        
    }
 
   return (
 
     <>
-     <div>      
+          
 
-      <form className="flex items-center justify-center mt-2">
+      <form onSubmit={handleSubmit} className="flex items-center justify-center mt-2">
 
          <input
           type="text"
-          value={formData}
-          name="input"
-          onChange={(e)=>setFormData(e.target.value)}
+          value={searchInput}
+          name="searchInput"  
+          onChange={handleChange}                
           className=" border-2 border-black rounded-lg "
          />
+
+         <label>
+          <input
+          type="radio"
+          name="type"
+          value="movie"
+          checked={type ==='movie'}
+          onChange={handleChange}
+           />
+           movie
+
+         </label>
+
+         <label>
+          <input
+          type="radio"
+          name="type"
+          value="tv"
+          checked={type ==='tv'}
+          onChange={handleChange}
+           />
+           Tv
+         </label>
+
          
-         <button onClick={handleSubmit}>search</button>  
+         
+         {/* <button onClick={handleSubmit}>search</button>   */}
         
                 
         
-      </form> 
-    </div>
-
-    
+      </form>  
     
     </>
    
