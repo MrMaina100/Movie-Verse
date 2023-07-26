fa5c48d4782760aa9d8ce7331f988148d191f6da
Derrick Maina <mburumaina648@gmail.com>
2023-07-26 16:48:29 +0300

    svg to jsx icon was added for the searchbar

---
+++ src/assests/SearchIcon.jsx
@@ -0,0 +1,21 @@
+function SearchIcon() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="1.5"
+      className="w-4.5 h-5"
+      viewBox="0 0 24 24"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+      ></path>
+    </svg>
+  );
+  
+  
+}
+export default SearchIcon
